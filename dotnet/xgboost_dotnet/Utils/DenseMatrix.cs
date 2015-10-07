323edede622ef507c5fa13434c4a7e26d4545587
Julian de Wit <julian@dwssystemen.nl>
2015-10-07 12:38:59 +0200

    Start dotnet wrapper

---
+++ dotnet/xgboost_dotnet/Utils/DenseMatrix.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xgboost_dotnet
+{
+    public class DenseMatrix
+    {
+        float[] _data;
+        int _colCount;
+        int _rowCount;
+
+        public DenseMatrix(int rowCount, int colCount)
+        {
+            _colCount = colCount;
+            _rowCount = rowCount;
+            _data = new float[rowCount * colCount];
+        }
+
+        public float this[int row, int col] 
+        {
+            get
+            {
+                return _data[row * _colCount + col];
+            }
+            set
+            {
+                _data[row * _colCount] = value;
+            }
+        }
+
+        public float[] Data
+        {
+            get { return _data; }
+        }
+
+        public int RowCount
+        {
+            get { return _rowCount; }
+        }
+
+        public int ColCount
+        {
+            get { return _colCount; }
+        }
+    }
+}
---
+++ dotnet/xgboost_dotnet/XgBoostWrapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xgboost_dotnet
+{
+    public static class XgBoostWrapper
+    {
+        public const string DLL_NAME = "xgboost_wrapper.dll";
+
+        //return 0 when success, -1 when failure happens
+        [DllImport(DLL_NAME)]
+        public static extern int XGDMatrixFree(IntPtr handle);
+
+        [DllImport(DLL_NAME)]
+        public static extern IntPtr XGDMatrixCreateFromFile(string fname, int silent);
+
+        //output = 0 when success, -1 when failure happens, return number of rows..
+        [DllImport(DLL_NAME)]
+        static extern int XGDMatrixNumRow(IntPtr handle, ref ulong output);
+
+        [DllImport(DLL_NAME)]
+        static extern IntPtr XGDMatrixCreateFromMat(float[] data, ulong nrow, ulong ncol, float missing);
+
+        public static int GetRowCount(IntPtr handle)
+        {
+            var error = 0ul;
+            var res = XgBoostWrapper.XGDMatrixNumRow(handle, ref error);
+            return res;
+        }
+
+        public static IntPtr CreateFromMatrix(float[] data, int rowCount, int colCount, float missing = float.MinValue)
+        {
+            var res = XgBoostWrapper.XGDMatrixCreateFromMat(data, (ulong)rowCount, (ulong)colCount, missing);
+            return res;
+        }
+
+    }
+}
